7a425a30c6ad516a5c0cd22b5a6014574abe6fcf
Chris Hartley <chris.hartley@anymouse.org>
2016-04-08 11:49:51 -0400

    forgot you

--- annual_report_form/models.py
+++ annual_report_form/models.py
@@ -10,8 +10,8 @@ def content_file_name(instance, filename):
 
 class annual_report(models.Model):
 
-    parcel = models.CharField(max_length=7, blank=False, null=False)
-    Property = models.ForeignKey(Property, blank=True, null=True)
+    parcel = models.CharField(max_length=7, blank=True, null=True)
+    Property = models.ForeignKey(Property, blank=False, null=True)
 
     name = models.CharField(max_length=254, blank=False, null=False)
     organization = models.CharField(max_length=254, blank=True, null=True)
@@ -33,18 +33,24 @@ class annual_report(models.Model):
 
     feedback = models.TextField(
         max_length=5120, help_text="Do you have any other comments on your project or feedback for Renew Indianapolis about your experience with our program?", verbose_name='Feedback')
+
+    STATE_CHOICES = (
+        (True, u'Yes'),
+        (False, u'No'),
+    )
+
     certificate_of_completion_ready = models.BooleanField(
-        verbose_name='Are you ready for a certificate of completion inspection?', default=False)
+        verbose_name='Are you ready for a certificate of completion inspection?', help_text="If you select Yes then someone from the city will contact you to schedule the inspection.", choices=STATE_CHOICES, default=False)
     property_occupied = models.BooleanField(
-        verbose_name='Is the property occupied?', default=False)
+        verbose_name='Is the property occupied?', choices=STATE_CHOICES, default=False)
 
     front_exterior_picture = models.ImageField(
-        upload_to=content_file_name, blank=True)
+        upload_to=content_file_name, blank=False)
     back_exterior_picture = models.ImageField(
-        upload_to=content_file_name, blank=True)
+        upload_to=content_file_name, blank=False)
     kitchen_picture = models.ImageField(
-        upload_to=content_file_name, blank=True)
+        upload_to=content_file_name, blank=False)
     bathroom_picture = models.ImageField(
-        upload_to=content_file_name, blank=True)
+        upload_to=content_file_name, blank=False)
     other_picture = models.ImageField(
         upload_to=content_file_name, blank=True, help_text='Project photo of your choice (optional)')
