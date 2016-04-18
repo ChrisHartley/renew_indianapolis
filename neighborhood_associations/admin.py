d861e8ad9c89b351f738ff4349a55003aead96d2
Chris Hartley <chris.hartley@anymouse.org>
2016-04-18 15:01:35 -0400

    embetter admin

--- neighborhood_associations/admin.py
+++ neighborhood_associations/admin.py
@@ -2,4 +2,7 @@ from django.contrib.gis import admin
 
 from .models import Neighborhood_Association
 
-admin.site.register(Neighborhood_Association, admin.OSMGeoAdmin)
+class Neighborhood_AssociationAdmin(admin.OSMGeoAdmin):
+    list_display = ('name', 'last_updated')
+    search_fields = ('name', 'contact_first_name', 'contact_last_name', 'contact_email_address', )
+admin.site.register(Neighborhood_Association, Neighborhood_AssociationAdmin)
