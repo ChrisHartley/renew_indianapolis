3eee68c42bd83e13b69af59482e685af028c2292
Chris Hartley <chris.hartley@anymouse.org>
2016-05-04 15:47:58 -0400

    add download link to admin

--- user_files/admin.py
+++ user_files/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from django.core.urlresolvers import reverse
+
 from .models import UploadedFile
 # Register your models here.
 class UploadedFileAdmin(admin.ModelAdmin):
     model = UploadedFile
     list_display = ('user','organization','application','supporting_document')
+    readonly_fields = ('file_download',)
+
+    def file_download(self, obj):
+        return mark_safe('<a href="{}">{}</a>'.format(
+            reverse("download_file", kwargs={'id':obj.id}),
+                "Download"
+            ))
 
 admin.site.register(UploadedFile,UploadedFileAdmin)
