ad9c47fc35a1d732dc491de1b7e3bbb0ab0bbffc
Chris Hartley <chris.hartley@anymouse.org>
2016-06-16 16:25:56 -0400

    admin wteak

--- annual_report_form/admin.py
+++ annual_report_form/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import annual_report
 
 class AnnualReportAdmin(admin.ModelAdmin):
-        list_display = ('Property', 'name', 'organization')
+        list_display = ('created','Property', 'name', 'organization')
         #list_filter = ()
         search_fields = ('name', 'organization', 'email', 'Property__parcel', 'Property__streetAddress')
         readonly_fields = ('Property',)
