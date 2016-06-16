21833e29f68b150eb191bcafd3f82654039771f8
Chris Hartley <chris.hartley@anymouse.org>
2016-06-16 16:28:13 -0400

    forgot to commit

---
+++ closings/templatetags/closings_extras.py
@@ -0,0 +1,16 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+from datetime import timedelta, datetime, date
+from num2words import num2words as num2words_orig
+
+register = template.Library()
+
+@register.filter
+def num2words(value):
+    """Returns a string/word representation of a number"""
+    return num2words_orig(value)
+
+@register.filter
+def plus_30_days(value):
+    """Returns now plus 30 days, an ugly hack"""
+    return date.today() + timedelta(days=30)
