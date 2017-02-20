2396dad78622ddb3796c5ee8365e5d8dd8e21d69
Chris Hartley <chris.hartley@anymouse.org>
2017-02-20 16:16:21 -0500

    rotate image to match EXIF data

--- photos/models.py
+++ photos/models.py
@@ -3,7 +3,7 @@ from property_inventory.models import Property
 from django.utils.text import slugify
 from os.path import basename
 from django.utils.safestring import mark_safe
-from PIL import Image
+from PIL import Image, ExifTags
 from django.conf import settings
 
 def save_location(instance, filename):
@@ -28,8 +28,20 @@ class photo(models.Model):
 
     def save(self, *args, **kwargs):
         super(photo, self).save(*args, **kwargs)
-        print self.image.path
+        #print self.image.path
         im = Image.open(self.image.path)
+        if hasattr(im, '_getexif'): # only present in JPEGs
+            for orientation in ExifTags.TAGS.keys():
+                if ExifTags.TAGS[orientation]=='Orientation':
+                    break
+        e = im._getexif()       # returns None if no EXIF data
+        if e is not None:
+            exif=dict(e.items())
+            orientation = exif[orientation]
+
+            if orientation == 3:   im = im.transpose(Image.ROTATE_180)
+            elif orientation == 6: im = im.transpose(Image.ROTATE_270)
+            elif orientation == 8: im = im.transpose(Image.ROTATE_90)
         im.thumbnail((1024,1024))
         im.save(self.image.path)
-        print "in save"
+        #print "in save"
