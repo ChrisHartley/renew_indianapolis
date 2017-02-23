7a0c8925de31d09f008ef9f68d35a6b9ab8cbbb8
Chris Hartley <chris.hartley@anymouse.org>
2017-02-22 20:18:07 -0500

    search property

--- photos/admin.py
+++ photos/admin.py
@@ -4,5 +4,6 @@ from .models import photo
 class PhotoAdmin(admin.ModelAdmin):
     fields = ( 'prop', 'main_photo', 'image', 'image_tag', 'created')
     readonly_fields = ('image_tag', 'created')
+    search_fields = ('prop',)
 
 admin.site.register(photo, PhotoAdmin)
