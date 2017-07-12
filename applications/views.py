b6fe207bbb43b96ac6d3daa4d575da5706915578
Chris Hartley <chris.hartley@anymouse.org>
2017-07-11 20:27:11 -0400

    ugh

--- applications/views.py
+++ applications/views.py
@@ -237,7 +237,7 @@ class CreateMeetingPriceChangeCMAArchive(View):
             with zipfile.ZipFile(tmp, 'w', zipfile.ZIP_DEFLATED) as myzip:
                 for price_change_link in meeting.price_change_meeting_link.all():
                     filename = str(price_change_link.price_change.cma.name)
-                    if filename is None:
+                    if price_change_link.price_change.cma.name is None:
                         pass
                     if filename.startswith('/') != True:
                         filename = settings.MEDIA_ROOT+filename
