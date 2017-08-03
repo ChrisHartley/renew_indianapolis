b997e7ffeb0af0eced566bf8cf476fa382bb67a4
Chris Hartley <chris.hartley@anymouse.org>
2017-08-03 12:53:14 -0400

    add search to admin, clean up map

--- surplus/admin.py
+++ surplus/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Parcel
 # Register your models here.
-admin.site.register(Parcel)
+
+class ParcelAdmin(admin.ModelAdmin):
+    search_fields = ['parcel_number', 'street_address', 'notes']
+
+
+admin.site.register(Parcel, ParcelAdmin)
