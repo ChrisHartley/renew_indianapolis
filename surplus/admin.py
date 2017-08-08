eaedddf04850f9c9a51e033163a42318ddcf866b
Chris Hartley <chris.hartley@anymouse.org>
2017-08-08 16:30:20 -0400

    tweak surplus admin view

--- surplus/admin.py
+++ surplus/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import Parcel
 # Register your models here.
 
+
+
 class ParcelAdmin(admin.ModelAdmin):
+    list_filter = ('has_building','classification','interesting','assessor_classification')
     search_fields = ['parcel_number', 'street_address', 'notes']
 
 
