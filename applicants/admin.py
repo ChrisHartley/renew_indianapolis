a25ca3033fc50368863d33c8270b16ea4c21c41a
Chris Hartley <chris.hartley@anymouse.org>
2017-08-09 13:26:55 -0400

    show application count in profile with link

--- applicants/admin.py
+++ applicants/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-from .models import ApplicantProfile, Organization
+from django.core.urlresolvers import reverse
+from django.utils.safestring import mark_safe
 
+from .models import ApplicantProfile, Organization
+from applications.models import Application
 
 class ApplicantProfileInline(admin.StackedInline):
     model = ApplicantProfile
@@ -23,9 +26,9 @@ class SimpleUserInline(admin.TabularInline):
 
 class ApplicantProfileAdmin(admin.ModelAdmin):
     model = ApplicantProfile
-    readonly_fields = ('user_email', 'user_first_name', 'user_last_name')
-    list_display = ('user_email', 'user_first_name', 'user_last_name', 'phone_number', 'mailing_address_line1', 'mailing_address_line2', 'mailing_address_line3', 'mailing_address_city', 'mailing_address_state', 'mailing_address_zip')
-    fields = ('user_email', 'user_first_name', 'user_last_name', 'phone_number', 'mailing_address_line1', 'mailing_address_line2', 'mailing_address_line3', 'mailing_address_city', 'mailing_address_state', 'mailing_address_zip')
+    readonly_fields = ('user_email', 'user_first_name', 'user_last_name', 'count_applications')
+    list_display = ('user_email', 'user_first_name', 'user_last_name', 'phone_number', 'mailing_address_line1', 'mailing_address_line2', 'mailing_address_line3', 'mailing_address_city', 'mailing_address_state', 'mailing_address_zip', 'count_applications')
+    fields = ('user_email', 'user_first_name', 'user_last_name', 'phone_number', 'mailing_address_line1', 'mailing_address_line2', 'mailing_address_line3', 'mailing_address_city', 'mailing_address_state', 'mailing_address_zip', 'count_applications')
     search_fields = ('user__email', 'phone_number', 'user__first_name', 'user__last_name')
 
     #inlines = [OrganizationInline]
@@ -38,6 +41,13 @@ class ApplicantProfileAdmin(admin.ModelAdmin):
 
     def user_email(self, obj):
         return obj.user.email
+
+    def count_applications(self, obj):
+        count = Application.objects.filter(user__exact=obj.user).exclude(status__exact=Application.INITIAL_STATUS).count()
+        summary_link = '<a href="{}">{}</a>'.format(
+            reverse("admin:app_list", args=('applications',))+'application/?q={}'.format(obj.user.email,), count)
+        return mark_safe(summary_link)
+    count_applications.short_description = 'Application count'
 
 
 class UserAdmin(UserAdmin):
