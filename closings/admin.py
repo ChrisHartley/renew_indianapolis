741a7e7385254301d05be9cea6bf230d615a71a9
Chris Hartley <chris.hartley@anymouse.org>
2017-11-24 10:50:07 -0500

    search parcel number too

--- closings/admin.py
+++ closings/admin.py
@@ -54,7 +54,7 @@ class ClosingAdmin(admin.ModelAdmin):
 
     form = ClosingAdminForm
     list_display = ['__unicode__','title_company','date_time', 'processing_fee_paid', 'city_documents_in_place', 'ri_documents_in_place', 'title_commitment_in_place', 'title_company_documents_in_place']
-    search_fields = ['prop__streetAddress', 'application__Property__streetAddress', 'application__user__first_name', 'application__user__last_name', 'application__user__email']
+    search_fields = ['prop__streetAddress', 'application__Property__streetAddress', 'prop__parcel', 'application__Property__parcel', 'application__user__first_name', 'application__user__last_name', 'application__user__email']
     list_filter = ('title_company', 'closed', PurchaseOptionFilter, ProccessingFeePaidFilter)
     readonly_fields = ('purchase_agreement', 'nsp', 'processing_fee_url', 'processing_fee_paid')
 
