a343d633a690fab3d4327a9078c71d1a9f3680db
Chris Hartley <chris.hartley@anymouse.org>
2017-12-20 13:56:46 -0500

    fix error on initial save of new closing, code may be overly complicated

--- closings/models.py
+++ closings/models.py
@@ -138,20 +138,23 @@ class closing(models.Model):
     ri_proceeds = models.DecimalField(max_digits=10, decimal_places=2, help_text="Amount for Renew Indianapolis", blank=True, null=True)
 
     def save(self, *args, **kwargs):
-        orig_closing = closing.objects.get(pk=self.pk)
-        if self.closed == False and self.assigned_city_staff is not None and self.application and settings.SEND_CLOSING_ASSIGNMENT_EMAILS:
-            if orig_closing.assigned_city_staff != self.assigned_city_staff:
-                subject = 'New closing assigned - {0} {1}'.format(self.application.Property, self.date_time)
-                message = 'Hello, the closing for {0} has been assigned to you, scheduled for {1} with {2}. Details and documents: https://www.renewindianapolis.org/map{3}'.format(self.application.Property, self.date_time, self.title_company, reverse('admin:closings_closing_proxy_change', args=(self.pk,)))
-                from_email = 'info@renewindianapolis.org'
-                send_mail(subject, message, from_email, [self.assigned_city_staff.email,settings.COMPANY_SETTINGS['APPLICATION_CONTACT_EMAIL']])
-
-        if self.closed == True and orig_closing.closed == False and self.application and self.application.Property.renew_owned == False and settings.SEND_CITY_CLOSED_NOTIFICATION_EMAIL:
-            subject = 'City owned property closed by Renew Indianapolis - {0}'.format(self.application.Property,)
-            message = 'This is a courtesy notification that the property at {0} was sold, please update your files as necessary.'.format(self.application.Property,)
-            from_email = 'info@renewindianapolis.org'
-            send_mail(subject, message, from_email, [settings.CITY_PROPERTY_MANAGER_EMAIL],)
+        try:
+            orig_closing = closing.objects.get(pk=self.pk)
+        except closing.DoesNotExist:
             pass
+        else:
+            if self.closed == False and self.assigned_city_staff is not None and self.application and settings.SEND_CLOSING_ASSIGNMENT_EMAILS:
+                if orig_closing.assigned_city_staff != self.assigned_city_staff:
+                    subject = 'New closing assigned - {0} {1}'.format(self.application.Property, self.date_time)
+                    message = 'Hello, the closing for {0} has been assigned to you, scheduled for {1} with {2}. Details and documents: https://www.renewindianapolis.org/map{3}'.format(self.application.Property, self.date_time, self.title_company, reverse('admin:closings_closing_proxy_change', args=(self.pk,)))
+                    from_email = 'info@renewindianapolis.org'
+                    send_mail(subject, message, from_email, [self.assigned_city_staff.email,settings.COMPANY_SETTINGS['APPLICATION_CONTACT_EMAIL']])
+
+            if self.closed == True and orig_closing.closed == False and self.application and self.application.Property.renew_owned == False and settings.SEND_CITY_CLOSED_NOTIFICATION_EMAIL:
+                subject = 'City owned property closed by Renew Indianapolis - {0}'.format(self.application.Property,)
+                message = 'This is a courtesy notification that the property at {0} was sold, please update your files as necessary.'.format(self.application.Property,)
+                from_email = 'info@renewindianapolis.org'
+                send_mail(subject, message, from_email, [settings.CITY_PROPERTY_MANAGER_EMAIL],)
 
         super(closing, self).save(*args, **kwargs)
         # we can only do fancy stuff if there is an application associated with the closing, which legacy closings don't have. so skip allt he fancy stuff in that case.
