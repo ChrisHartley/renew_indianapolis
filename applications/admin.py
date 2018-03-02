deac8a44e71cfc1e09d9c2e46434a6e6cf38d29d
Chris Hartley <chris.hartley@anymouse.org>
2018-03-02 16:35:17 -0500

    show submitted time

--- applications/admin.py
+++ applications/admin.py
@@ -71,7 +71,7 @@ class NeighborhoodNotificationAdmin(admin.TabularInline):
         return formfield
 
 class ApplicationAdmin(admin.ModelAdmin, ExportMixin):
-    list_display = ('modified','Property', 'num_scheduled_apps', 'user_link', 'organization','application_type','scheduled_meeting', 'status')
+    list_display = ('modified','submitted_timestamp','Property', 'num_scheduled_apps', 'user_link', 'organization','application_type','scheduled_meeting', 'status')
     list_filter = ('status','application_type')
     search_fields = ('Property__parcel', 'Property__streetAddress', 'user__email', 'user__first_name', 'user__last_name', 'organization__name')
     readonly_fields = ('created', 'modified', 'user_readable', 'property_type', 'property_status','property_vacant_lot','property_sidelot','scheduled_meeting','application_summary_page','application_detail_page','n_notification', 'submitted_timestamp', 'price_at_time_of_submission')
