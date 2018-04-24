85b76b6727fb71298ecbf2dc35ea6bf721f783b5
Chris Hartley <chris.hartley@anymouse.org>
2018-04-24 14:36:31 -0400

    require fields

--- closings/models.py
+++ closings/models.py
@@ -134,8 +134,8 @@ class closing(models.Model):
     assigned_city_staff = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, limit_choices_to={'groups__name__in': ["City Staff",]})
     closed = models.BooleanField(default=False, help_text="Has this transaction been completed?")
     notes = models.CharField(max_length=2048, blank=True, help_text="Internal notes")
-    city_proceeds = models.DecimalField(max_digits=10, decimal_places=2, help_text="Amount for the City of Indianapolis", blank=True, default=0)
-    city_loan_proceeds = models.DecimalField(max_digits=10, decimal_places=2, help_text="Amount due to the City of Indianapolis, but not paid at closing", blank=True, default=0)
+    city_proceeds = models.DecimalField(max_digits=10, decimal_places=2, help_text="Amount for the City of Indianapolis", blank=False, default=0)
+    city_loan_proceeds = models.DecimalField(max_digits=10, decimal_places=2, help_text="Amount due to the City of Indianapolis, but not paid at closing", blank=False, default=0)
     ri_proceeds = models.DecimalField(max_digits=10, decimal_places=2, help_text="Amount for Renew Indianapolis", blank=True, default=0)
 
     archived = models.BooleanField(default=False, help_text="Closing did not occur and should be archived.")
