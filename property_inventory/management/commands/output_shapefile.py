c2142e77692b925fb2fdbaba6c97711f30b51402
Chris Hartley <chris.hartley@renewindianapolis.org>
2018-10-05 15:25:16 -0400

    blah

--- property_inventory/management/commands/output_shapefile.py
+++ property_inventory/management/commands/output_shapefile.py
@@ -14,13 +14,7 @@ class Command(BaseCommand):
         user = settings.DATABASES['default']['USER']
         pw = settings.DATABASES['default']['PASSWORD']
         tmp_dir = mkdtemp()
-        try:
+        check_call(['pgsql2shp',  '-u', user, '-P', pw, '-f', '{0}/renew_inventory.shp'.format(tmp_dir,), db, query])
 
-            check_call(['pgsql2shp',  '-u', user, '-P', pw, '-f', '{0}/renew_inventory.shp'.format(tmp_dir,), db, query])
-        except CalledProcessError:
-            raise CalledProcessError()
-            # sigh I don't know, it just seemed like we shoudl acknowledge the error, even if we want to freak out about it
-
-        #check_call(['zip', '-D', '/tmp/renew_inventory.zip', '/tmp/renew_inventory.dbf','/tmp/renew_inventory.prj','/tmp/renew_inventory.shp', '/tmp/renew_inventory.shx', '/tmp/renew_inventory.cpg'])
         make_archive('static/renew_inventory', 'zip', tmp_dir)
         rmtree(tmp_dir)
