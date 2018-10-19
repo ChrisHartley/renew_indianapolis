9c61a954ff5474391110c64dc55cb9adfa2bf769
Chris Hartley <chris.hartley@renewindianapolis.org>
2018-10-19 09:48:42 -0400

    unicode this string

--- property_inquiry/admin.py
+++ property_inquiry/admin.py
@@ -82,7 +82,7 @@ class propertyInquiryAdmin(admin.ModelAdmin):
     def user_name(self, obj):
 #        email_link = '<a target="_blank" href="https://mail.google.com/a/landbankofindianapolis.org/mail/u/1/?view=cm&fs=1&to={0}&su={1}&body={2}&tf=1">{3}</a>'.format(obj.user.email, 'Property visit: '+str(obj.Property), 'Hi ' +obj.user.first_name+',', obj.user.email)
         email_link = ''
-        name_link = '<a href="{}">{}</a>'.format(
+        name_link = u'<a href="{}">{}</a>'.format(
              reverse("admin:applicants_applicantprofile_change", args=(obj.user.profile.id,)),
                  obj.user.first_name + ' ' + obj.user.last_name
              )
