e56b6323f870e32837e0aaf6d7de08df2236cd6d
Chris Hartley <chris.hartley@renewindianapolis.org>
2019-05-13 11:33:57 -0400

    handle new property creation without error

--- property_inventory/admin.py
+++ property_inventory/admin.py
@@ -134,8 +134,11 @@ class PropertyAdmin(admin.OSMGeoAdmin, ExportCsvMixin):
         return inquiries
 
     def view_photos(self, obj):
-        photo_page_link = '<a href="{}">{}</a>'.format(
-                    reverse("property_photos", kwargs={'parcel': obj.parcel}), "View Photos")
+        if self.id:
+            photo_page_link = '<a href="{}">{}</a>'.format(
+                        reverse("property_photos", kwargs={'parcel': obj.parcel}), "View Photos")
+        else:
+            photo_page_link = '-'
         return mark_safe(photo_page_link)
 
     def condition_report_link(self, obj):
