b9040eaaa0d93b3a371fb4e2956cfcd84295272b
Chris Hartley <chris.hartley@renewindianapolis.org>
2019-05-14 10:41:29 -0400

    correct typo in fields

--- photos/admin.py
+++ photos/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import photo
 # Register your models here.
 class PhotoAdmin(admin.ModelAdmin):
-    fields = ( 'prop', 'prop_ncst' 'main_photo', 'image', 'image_tag', 'created')
+    fields = ( 'prop', 'prop_ncst', 'main_photo', 'image', 'image_tag', 'created')
     readonly_fields = ('image_tag', 'created')
     search_fields = ( 'prop__streetAddress', 'prop__parcel', 'prop_ncst__street_address', 'prop_ncst__parcel')
     list_display = ('prop', 'prop_ncst', 'image', 'main_photo')
