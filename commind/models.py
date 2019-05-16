e8b538c437472e98d114d25cb210ff5e00c7b3b7
Chris Hartley <chris.hartley@renewindianapolis.org>
2019-05-16 13:42:38 -0400

    revamp application

--- commind/models.py
+++ commind/models.py
@@ -236,7 +236,9 @@ class Application(models.Model):
     )
 
 
-    Properties = models.ManyToManyField(Property)
+    Properties = models.ManyToManyField(Property, blank=True)
+    Property = models.ForeignKey(Property, null=True, blank=True, related_name='application_tmp')
+
     user = models.ForeignKey(get_user_model(), related_name='commind_app')
 
     status = models.IntegerField(
@@ -333,11 +335,21 @@ class Application(models.Model):
         blank=False,
     )
 
+    total_costs = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        help_text="Total estimated construction and/or rehabilitation costs",
+        null=True,
+        blank=False,
+    )
+
     proposed_end_use = models.CharField(
         max_length=512,
         blank=False,
         help_text="What is the proposed end use of the property?",
     )
+
+#    skills_and_expierence_applicant = models.
 
 
     applicant_work_character = models.CharField(
@@ -376,7 +388,181 @@ class Application(models.Model):
         help_text="List any current community, neighborhood and/or charitable partnerships in relation to this project",
     )
 
+    applicant_skills_environmental_remediation = models.CharField(
+        max_length=254,
+        verbose_name='Environmental remediation',
+        blank=True,
+    )
+    applicant_skills_brownfields = models.CharField(
+        max_length=254,
+        verbose_name='Brownfields',
+        blank=True,
+    )
+    applicant_skills_commercial_real_estate_development = models.CharField(
+        max_length=254,
+        verbose_name='Commercial real estate development',
+        blank=True,
+    )
+    applicant_skills_entitlement_process = models.CharField(
+        max_length=254,
+        verbose_name='Entitlement process',
+        blank=True,
+    )
+    applicant_skills_incentives = models.CharField(
+        max_length=254,
+        verbose_name='Incentives',
+        blank=True,
+    )
+    applicant_skills_other = models.CharField(
+        max_length=254,
+        verbose_name='Other',
+        blank=True,
+    )
+    applicant_skills_environmental_remediation_boolean = models.NullBooleanField(
+        default=None,
+        blank=False,
+        verbose_name='Environmental remediation',
+    )
+    applicant_skills_brownfields_boolean = models.NullBooleanField(
+        default=None,
+        blank=False,
+        verbose_name='Brownfields',
+    )
+    applicant_skills_commercial_real_estate_development_boolean = models.NullBooleanField(
+        default=None,
+        blank=False,
+        verbose_name='Commercial real estate development',
+    )
+    applicant_skills_entitlement_process_boolean = models.NullBooleanField(
+        default=None,
+        blank=False,
+        verbose_name='Entitlement process',
+    )
+    applicant_skills_incentives_boolean = models.NullBooleanField(
+        default=None,
+        blank=False,
+        verbose_name='Incentives',
+    )
+    applicant_skills_other_boolean = models.NullBooleanField(
+        default=None,
+        blank=False,
+        verbose_name='Other (fill in the blank)',
+    )
 
+
+
+    NEW_CONSTRUCTION_CHOICE = 1
+    REHABILITATION_CHOICE = 2
+    BOTH_CHOICE = 3
+
+    PROJECT_TYPE_CHOICES = (
+        (NEW_CONSTRUCTION_CHOICE,'New construction'),
+        (REHABILITATION_CHOICE,'Rehabilitation'),
+        (BOTH_CHOICE,'New construction and rehabilitation'),
+    )
+
+
+    project_type = models.IntegerField(
+        choices=PROJECT_TYPE_CHOICES,
+        blank=True,
+        null=True,
+        verbose_name='Will your project involve:',
+    )
+    allignment_with_current_plans = models.NullBooleanField(
+        choices=YESNO_TYPES,
+        blank=True,
+        null=True,
+        verbose_name='Is your project/development in alignment with current plans (i.e. neighborhood plans, long term plans, city plans for land use, etc.)?',
+    )
+    allignment_with_current_plans_details = models.CharField(
+        max_length=1024,
+        blank=True,
+        help_text="Please provide details",
+    )
+    stakeholders_contacted = models.NullBooleanField(
+        choices=YESNO_TYPES,
+        blank=True,
+        null=True,
+        verbose_name='Have you contacted any community and/or neighborhood stakeholders in regard to your project?',
+    )
+    stakeholders_contacted_details = models.CharField(
+        max_length=1024,
+        blank=True,
+        help_text="Please provide details",
+    )
+
+    total_number_of_jobs = models.PositiveIntegerField(
+        blank=True,
+        null=True,
+        verbose_name='The total number of jobs',
+    )
+
+    total_annual_new_salaries = models.FloatField(
+        blank=True,
+        null=True,
+        verbose_name='The total dollar amount to be annually spent on new salaries',
+    )
+
+    blended_hourly_wage_rate = models.FloatField(
+        blank=True,
+        null=True,
+        verbose_name='What is the blended hourly wage rate (the blended rate of hourly and salaried employees) excluding benefits and overtime, for the jobs that will be created by this project?'
+    )
+
+    hired_participant = models.NullBooleanField(
+        blank=True,
+        null=True,
+        verbose_name="Does this project wish to participate in the City’s Hiring Incentive for Re-Entry Development (HIRED) Program?",
+    )
+
+    low_mod_daycare = models.NullBooleanField(
+        blank=True,
+        null=True,
+        verbose_name="Is there opportunity with this project to provide low to mod income employees with daycare support?",
+    )
+    low_mod_transportation_support = models.NullBooleanField(
+        blank=True,
+        null=True,
+        verbose_name="Is there opportunity with this project to provide low to mod income employees with transportation support?",
+    )
+
+    youth_employment_opportunity = models.NullBooleanField(
+        blank=True,
+        null=True,
+        verbose_name="Is there opportunity with this project for youth employeement?",
+    )
+    neighborhood_hiring_opportunity = models.NullBooleanField(
+        blank=True,
+        null=True,
+        verbose_name="Is there opportunity with this project for hiring employees within the neighborhood?",
+    )
+    subbaccalaureate_training_opportunity = models.NullBooleanField(
+        blank=True,
+        null=True,
+        verbose_name="Is there opportunity with this project for training for sub-baccalaureate candidates?",
+    )
+
+    arts_consideration = models.NullBooleanField(
+        blank=True,
+        null=True,
+        verbose_name="Is there an arts consideration with this project?",
+    )
+    arts_consideration_explanation = models.CharField(
+        max_length=1024,
+        blank=True,
+        verbose_name="If yes, please explain further"
+    )
+
+    minority_owned_business_question = models.NullBooleanField(
+        blank=True,
+        null=True,
+        verbose_name="Will this project be developed by, owned by or engage with any minority owned businesses and/or entities and/or an equal opportunity employer?",
+    )
+    minority_owned_business_question_detail = models.CharField(
+        max_length=1024,
+        blank=True,
+        verbose_name="If yes, please explain further"
+    )
 
     documents = GenericRelation(Document, related_query_name='application')
     notes = GenericRelation(Note, related_query_name='application_notes')
@@ -455,4 +641,8 @@ class Application(models.Model):
         super(Application, self).save(*args, **kwargs)
 
     def __str__(self):
-        return '{} - {}'.format(self.created, self.user)
+        return '{} - {} {} - {} - {}'.format(self.Properties.first(), self.user.first_name, self.user.last_name, self.entity, self.get_status_display())
+
+    def __iter__(self):
+        for field in self._meta.fields:
+            yield (field.verbose_name, field.value_to_string(self))
