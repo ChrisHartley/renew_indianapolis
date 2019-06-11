148c9c46d18872b5bb2122c4e79716afbe9e8f69
Chris Hartley <chris.hartley@renewindianapolis.org>
2019-06-11 14:47:14 -0400

    typo

--- property_inquiry/models.py
+++ property_inquiry/models.py
@@ -57,7 +57,7 @@ class propertyInquiry(models.Model):
         verbose_name_plural = "property inquiries"
 
     def __unicode__(self):
-        return u'{0} - {1} {2} ({3}) - {4}'.format(self.Property, self.user.first_name, self.user.last_name, self.user.email, self.timestamp.strftime('%x'), self.get_status_display())
+        return u'{0} - {1} {2} ({3}) - {4} - {5}'.format(self.Property, self.user.first_name, self.user.last_name, self.user.email, self.timestamp.strftime('%x'), self.get_status_display())
 
 def save_location(instance, filename):
     return "property_showing/{0}/{1}/{2}".format(instance.Property, instance.datetime.strftime('%Y-%m-%d'), filename)
