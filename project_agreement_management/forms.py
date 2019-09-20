6461fa29e11305f979639f6066a71c76fcd1d5b8
Chris Hartley <chris.hartley@renewindianapolis.org>
2019-09-20 11:36:20 -0400

    tax delinquency management command for pa management

--- project_agreement_management/forms.py
+++ project_agreement_management/forms.py
@@ -3,6 +3,10 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from .models import InspectionRequest
 from property_inventory.models import Property
+from django.conf import settings
+from django.urls import reverse
+from django.core.mail import send_mail
+
 
 class InspectionRequestForm(forms.ModelForm):
     note = forms.CharField(
@@ -11,7 +15,16 @@ class InspectionRequestForm(forms.ModelForm):
         widget=forms.Textarea
     )
 
-    
+    def send_email(self, prop):
+        subject = 'New inspection request - {0}'.format(prop,)
+        message = 'Hello, an inspection for {0} has been submitted. View here: https://build.renewindianapolis.org{1}'.format(prop, reverse('admin:project_agreement_management_inspectionrequest_changelist'))#, args=(self.pk,)))
+        from_email = 'info@renewindianapolis.org'
+        if prop.renew_owned == True:
+            to_email = settings.COMPANY_SETTINGS['RENEW_PA_RELEASE']
+        else:
+            to_email = settings.COMPANY_SETTINGS['CITY_PA_RELEASE']
+        send_mail(subject, message, from_email, [to_email,])
+
 
     def __init__(self, *args, **kwargs):
         super(InspectionRequestForm, self).__init__(*args, **kwargs)
--- project_agreement_management/views.py
+++ project_agreement_management/views.py
@@ -5,12 +5,13 @@ from django.shortcuts import render
 from django.views.generic.edit import FormView
 from .forms import InspectionRequestForm
 from property_inventory.models import Property
-
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 class InspectionRequestFormView(FormView):
     template_name = 'project_agreement_management/inspection_request_form.html'
     form_class = InspectionRequestForm
-    success_url = ''
+    success_url = reverse_lazy('applicants_home')
 
     def get_initial(self):
         initial = super(InspectionRequestFormView, self).get_initial()
@@ -21,5 +22,6 @@ class InspectionRequestFormView(FormView):
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
-        form.send_email()
-        return super(ContactView, self).form_valid(form)
+        form.send_email(form.instance.Property)
+        messages.add_message(self.request, messages.INFO, "Inspection request submitted!")
+        return super(InspectionRequestFormView, self).form_valid(form)
