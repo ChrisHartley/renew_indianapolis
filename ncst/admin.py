947fff18b139b8f37e5ea9ab01596b40a41e5382
Chris Hartley <chris.hartley@renewindianapolis.org>
2019-10-03 15:53:53 -0400

    add inventory sales within census tract field

--- ncst/admin.py
+++ ncst/admin.py
@@ -11,6 +11,7 @@ from utils.admin import ExportCsvMixin
 from .models import Contact, Property, Program, Seller
 from photos.models import photo
 from property_condition.models import ConditionReport
+from property_inventory.models import census_tract, Property as InventoryProperty
 
 class photoInlineAdmin(regular_admin.TabularInline):
     model = photo
@@ -37,7 +38,15 @@ class PropertyAdmin(admin.OSMGeoAdmin, ExportCsvMixin):
     search_fields = ('parcel', 'street_address', 'zipcode')
     list_display = ('parcel', 'street_address', 'status', 'lockbox', 'recommendation', 'price_offer', 'condition_report_completed')
     list_filter = ('status',)
+    readonly_fields = ('census_tract_landbank_sales_count', )
     inlines = [ photoInlineAdmin,propertyConditionReportInlineAdmin]
+
+    def census_tract_landbank_sales_count(self, obj):
+        # return count of inventory sales within this census tract
+        ct = census_tract.objects.filter(geometry__contains=obj.geometry.centroid).first()
+        p = InventoryProperty.objects.filter(geometry__contained=ct.geometry).filter(status__startswith='Sold').count()
+        return p
+   census_tract_landbank_sales_count.short_description = 'Sold landbank properties within census tract'
 
     def condition_report_completed(self, obj):
         return ConditionReport.objects.filter(Property_ncst=obj).count()>0
