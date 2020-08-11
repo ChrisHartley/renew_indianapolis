eca7a3fc47d226fe44b44a11e46240ca2f376ef6
Chris Hartley <chris.hartley@renewindianapolis.org>
2020-08-11 09:56:55 -0400

    handle 3 types of properties

--- photos/forms.py
+++ photos/forms.py
@@ -6,9 +6,9 @@ from .models import photo
 
 class DumpPhotosForm(forms.Form):
     #prop = forms.ModelChoiceField(queryset=Property.objects.filter(status__exact='Available').filter(structureType__contains='Residential Dwelling').order_by('streetAddress'))
-    prop = forms.ModelChoiceField(queryset=Property.objects.all())
-    prop_surplus = forms.ModelChoiceField(queryset=SurplusProperty.objects.all())
-    prop_ncst = forms.ModelChoiceField(queryset=NCSTProperty.objects.all())
+    prop = forms.ModelChoiceField(queryset=Property.objects.all(), required=False)
+    prop_surplus = forms.ModelChoiceField(queryset=SurplusProperty.objects.all(), required=False)
+    prop_ncst = forms.ModelChoiceField(queryset=NCSTProperty.objects.all(), required=False)
     image1 = forms.ImageField(label="image 1", required=False)
     image2 = forms.ImageField(label="image 2", required=False)
     image3 = forms.ImageField(label="image 3", required=False)
@@ -38,6 +38,18 @@ class DumpPhotosForm(forms.Form):
     )
     primary_image = forms.ChoiceField(choices=IMAGE_CHOICES)
 
+    def clean(self):
+        cleaned_data = super(DumpPhotosForm, self).clean()
+        prop = cleaned_data.get('prop')
+        prop_surplus = cleaned_data.get('prop_surplus')
+        prop_ncst = cleaned_data.get('prop_ncst')
+        if prop is None and prop_surplus is None and prop_ncst is None:
+            msg = "Must specify one property"
+            self.add_error('prop', msg)
+            self.add_error('prop_surplus', msg)
+            self.add_error('prop_ncst', msg)
+
+
     def save_photos(self):
         for image, number in ( (self.cleaned_data.get('image1'), 1), (self.cleaned_data.get('image2'), 2), (self.cleaned_data.get('image3'), 3), (self.cleaned_data.get('image4'), 4), (self.cleaned_data.get('image5'), 5), (self.cleaned_data.get('image6'), 6), (self.cleaned_data.get('image7'), 7), (self.cleaned_data.get('image8'), 8), (self.cleaned_data.get('image9'), 9), (self.cleaned_data.get('image10'), 10) ):
             if number == int(self.cleaned_data.get('primary_image')):
@@ -45,5 +57,14 @@ class DumpPhotosForm(forms.Form):
             else:
                 prime = False
             if image is not None:
-                p = photo(image=image, prop=self.cleaned_data.get('prop'), main_photo=prime)
+                prop = self.cleaned_data.get('prop')
+                prop_surplus = self.cleaned_data.get('prop_surplus')
+                prop_ncst = self.cleaned_data.get('prop_ncst')
+                if prop is not None:
+                    s = prop
+                if prop_surplus is not None:
+                    s = prop_surplus
+                if prop_ncst is not None:
+                    s = prop_ncst
+                p = photo(image=image, prop=s, main_photo=prime)
                 p.save()
