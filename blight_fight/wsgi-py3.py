4770c0b6752d8a57b54e5de5f64c8c9e058c0a1b
Chris Hartley <chris.hartley@renewindianapolis.org>
2021-04-29 07:39:53 -0400

    try

--- blight_fight/wsgi-py3.py
+++ blight_fight/wsgi-py3.py
@@ -16,24 +16,24 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'blight_fight.settings'
 # in production server we have an extra settings file that overwrites devel settings.
 # If we can import it then we are on production server and need to set up
 # the virtualenv. Sucks that these paths are hard-coded.
-try:
-    # Add the site-packages of the chosen virtualenv to work with
-    site.addsitedir(
-        '/home/django/.virtualenvs/bfpy3/lib/python3.6/site-packages')
+#try:
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir(
+    '/home/django/.virtualenvs/bfpy3/lib/python3.6/site-packages')
 
-    # Add the app's directory to the PYTHONPATH
-    sys.path.append('/home/django/blight_fight')
-    sys.path.append('/home/django/blight_fight/blight_fight')
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/home/django/blight_fight')
+sys.path.append('/home/django/blight_fight/blight_fight')
 
-    from settings_production import *
+from settings_production import *
 
-    # Activate your virtual env
-    activate_env = os.path.expanduser(
-        "/home/django/.virtualenvs/bfpy3/bin/activate_this.py")
-    exec(compile(open(activate_env).read(), activate_env, 'exec'), dict(__file__=activate_env))
+# Activate your virtual env
+activate_env = os.path.expanduser(
+    "/home/django/.virtualenvs/bfpy3/bin/activate_this.py")
+exec(compile(open(activate_env).read(), activate_env, 'exec'), dict(__file__=activate_env))
 
-except ImportError:
-    pass
+#except ImportError:
+#    pass
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
